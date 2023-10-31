6a698b3ff112db24700e1147bbe9f7279ce04a79
Turbstructor <ycw0201@naver.com>
2023-10-31 19:04:54 +0900

    Add: reworked Assignment01

---
+++ assignments/assignment01/src/main/java/club/pard/server/assignment01/controller/MainController.java
@@ -0,0 +1,37 @@
+package club.pard.server.assignment01.controller;
+
+
+// TODO: MainController를 만들어주세요.
+
+import club.pard.server.assignment01.dto.MyInfoDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/start")
+    public String startPage() { return "start"; }
+
+
+    @PostMapping("/showInfo")
+    public String showForm(MyInfoDto dto, Model model) {
+        model.addAttribute("major", dto.getMajor());
+        model.addAttribute("grade", dto.getGrade());
+        model.addAttribute("age", dto.getAge());
+        model.addAttribute("hobby", dto.getHobby());
+        model.addAttribute("name", dto.getName());
+        model.addAttribute("hometown", dto.getHometown());
+        model.addAttribute("introduction", dto.getIntroduction());
+
+        return "showInfo";
+    }
+
+    @GetMapping("/submitInfo")
+    public String newInfoForm() {
+        return "submitInfo";
+    }
+
+}
---
+++ assignments/assignment01/src/test/java/club/pard/server/assignment01/Assignment01ApplicationTests.java
@@ -0,0 +1,13 @@
+package club.pard.server.assignment01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
