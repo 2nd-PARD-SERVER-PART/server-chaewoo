1e6133c581fb7d6e36e0c3fedbc7f021f8b228d1
Turbstructor <ycw0201@naver.com>
2023-11-02 17:10:51 +0900

    Add: reworked assignment02

---
+++ assignments/assignment02/src/main/java/club/pard/server/assignment02/controller/ItemController.java
@@ -0,0 +1,69 @@
+package club.pard.server.assignment02.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import club.pard.server.assignment02.model.Item;
+import club.pard.server.assignment02.repository.ItemRepository;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+    private final ItemRepository itemRepository;
+
+    public ItemController(ItemRepository itemRepository){ this.itemRepository = itemRepository; }
+
+    //////////
+    // Creating
+    @PostMapping("/add") public String addItem(@RequestBody Item item)
+    {
+        itemRepository.add(item);
+        return String.format("Item #%d added.", item.getId());
+    }
+
+
+    //////////
+    // Reading
+    @GetMapping("/list")
+    public List<Item> getItemList(){ return itemRepository.findAll(); }
+
+    @GetMapping("/list/{targetId}")
+    public Item getItemById(@RequestParam Integer targetId){ return itemRepository.findById(targetId); }
+
+
+    //////////
+    // Updating
+    @PatchMapping("/update/{targetId}")
+    public String updateItem(@RequestParam Integer targetId, @RequestBody Item newItem)
+    {
+        Item targetItem = itemRepository.findById(targetId);
+
+        if(targetItem != null)
+        {
+            targetItem.setName(newItem.getName());
+            targetItem.setPrice(newItem.getPrice());
+            targetItem.setQuantity(newItem.getQuantity());
+
+            return String.format("Item #%d updated", targetId);
+        }
+        else return String.format("Failed to find target item #%d", targetId);
+    }
+
+
+    //////////
+    // Deleting
+    @DeleteMapping("/remove/{targetId}")
+    public String removeItem(@RequestParam Integer targetId)
+    {
+        if(itemRepository.delete(targetId)) return String.format("Item #%d removed from the list.", targetId);
+        else return "Failed to retrieve item.";
+    }
+}
---
+++ assignments/assignment02/src/main/java/club/pard/server/assignment02/repository/ItemRepository.java
@@ -0,0 +1,42 @@
+package club.pard.server.assignment02.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import club.pard.server.assignment02.model.Item;
+
+@Repository
+public class ItemRepository {
+    private static final Map<Integer, Item> items = new HashMap<>();
+
+    public Item add(Item item)
+    {
+        items.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Integer itemId){ return items.get(itemId); }
+    public List<Item> findAll(){ return new ArrayList<>(items.values()); }
+
+    public boolean update(Integer id, Item newItem)
+    {
+        Item target = this.findById(id);
+
+        if(target != null)
+        {
+            target.setId(newItem.getId());
+            target.setName(newItem.getName());
+            target.setPrice(newItem.getPrice());
+            target.setQuantity(newItem.getQuantity());
+
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean delete(Integer id){ return (items.remove(id) != null); }
+}
